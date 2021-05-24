f5da99007b02f1dfbef6a6864f593a3281efef0b
Wang David <wangdavid@WangdeMacBook-Pro.local>
2021-05-24 19:31:00 +0800

    add

---
+++ src/main/java/com/david/algo/bit/MaximizeXor_1707.java
@@ -0,0 +1,13 @@
+package com.david.algo.bit;
+
+/**
+ * TODO
+ * 1707. 与数组中元素的最大异或值
+ */
+public class MaximizeXor_1707 {
+    class Solution {
+        public int[] maximizeXor(int[] nums, int[][] queries) {
+            return null;
+        }
+    }
+}
