7209e57be44e107c53fe886b64eee81097554225
david <david@ningmengjingdeMacBook-Air.local>
2021-06-15 21:27:57 +0800

    add

--- src/main/java/com/david/algo/bsearch/PeakIndexInMountainArray_852.java
+++ src/main/java/com/david/algo/bsearch/PeakIndexInMountainArray_852.java
@@ -1,4 +1,4 @@
-package com.david.algo.basic;
+package com.david.algo.bsearch;
 
 /**
  * 输入：arr = [0,2,1,0]
@@ -8,19 +8,31 @@ public class PeakIndexInMountainArray_852 {
     public static void main(String[] args) {
         System.out.println(new PeakIndexInMountainArray_852().new Solution().peakIndexInMountainArray(new int[]{0, 1, 0}));
         System.out.println(new PeakIndexInMountainArray_852().new Solution1().peakIndexInMountainArray(new int[]{0, 1, 0}));
-        ;
+        System.out.println(new PeakIndexInMountainArray_852().new Solution2().peakIndexInMountainArray(new int[]{0, 1, 0}));
+    }
+
+    class Solution2 {
+        public int peakIndexInMountainArray(int[] arr) {
+            int n = arr.length;
+            if (n < 3) return -1;
+            for (int l = 1, r = n - 2; l <= r; ) {
+                int mid = l + (r - l >> 1);
+                if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) return mid;
+                if (arr[mid] > arr[mid + 1] && arr[mid] < arr[mid - 1]) r = mid - 1;
+                else l = mid + 1;
+            }
+            return -1;
+        }
     }
 
     /**
      * 二分法
-     * O(logn
-     *
-     * )
+     * O(logn)
      */
     class Solution1 {
         public int peakIndexInMountainArray(int[] arr) {
             int n = arr.length;
-            if (n < 3) return -1;
+            if (n < 3) return -1;//组不成山峰
             for (int l = 1, r = n - 2; l <= r; ) {
                 int mid = l + (r - l) / 2;
                 if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) return mid;
